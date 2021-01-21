c5de91723f36a08aee747832c71ec9003ada93d3
Cat Chen <catchen@catchen.me>
2021-01-20 23:15:14 -0800

    Created validation script to prevent extra properties (#15)

---
+++ validate.ts
@@ -0,0 +1,60 @@
+import Ajv, { JSONSchemaType, DefinedError } from "ajv";
+import type { Opening, Job, Company, JobsData } from "./index";
+import data from "./data.json";
+
+const openingSchema: JSONSchemaType<Opening> = {
+  type: "object",
+  required: ["available"],
+  properties: {
+    available: { type: "boolean" },
+    url: { type: "string", nullable: true },
+  },
+  additionalProperties: false,
+};
+
+const jobSchema: JSONSchemaType<Job> = {
+  type: "object",
+  required: [],
+  properties: {
+    intern: { ...openingSchema, nullable: true },
+    newGrad: { ...openingSchema, nullable: true },
+    senior: { ...openingSchema, nullable: true },
+    manager: { ...openingSchema, nullable: true },
+  },
+  additionalProperties: false,
+};
+
+const companySchema: JSONSchemaType<Company> = {
+  type: "object",
+  required: ["jobs"],
+  properties: {
+    url: { type: "string", nullable: true },
+    jobs: {
+      type: "object",
+      required: [],
+      properties: {
+        softwareEngineer: { ...jobSchema, nullable: true },
+        dataScientist: { ...jobSchema, nullable: true },
+        productManager: { ...jobSchema, nullable: true },
+      },
+      additionalProperties: false,
+    },
+  },
+  additionalProperties: false,
+};
+
+const schema: JSONSchemaType<JobsData> = {
+  type: "object",
+  required: [],
+  additionalProperties: companySchema,
+};
+
+// @ts-ignore
+const ajv: Ajv = new Ajv.default();
+const validate = ajv.compile(schema);
+if (!validate(data)) {
+  for (const error of validate.errors as DefinedError[]) {
+    console.error(error);
+  }
+  throw new Error("Validation failure");
+}
