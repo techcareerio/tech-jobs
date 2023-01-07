a83cb0bbe6e5208ec2d2cab5914d8c7e67c4a0ed
Cat Chen <catchen@catchen.me>
2023-01-07 15:44:40 -0800

    Set up VS Code settings to use the schema when editing data.json

--- validate.ts
+++ validate.ts
@@ -1,9 +1,9 @@
 import Ajv, { DefinedError, Schema } from 'ajv';
 import data from './data.json' assert { type: 'json' };
-import schemas from './schemas.json' assert { type: 'json' };
+import schema from './schema.json' assert { type: 'json' };
 
 const ajv = new Ajv();
-ajv.addSchema(schemas as Schema);
+const validate = ajv.addSchema(schema as Schema);
 if (!ajv.validate('#/definitions/JobsData', data)) {
   for (const error of ajv.errors as DefinedError[]) {
     console.error(error);
